9c766dbb402a30386eee79bc6ab66da81e8785f6
Georg Leciejewski <gl@salesking.eu>
2012-07-08 21:00:48 +0200

    add filter for objects

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,44 @@
 $(document).ready(function () {
 
+  var anchor = window.location.hash.substring(1);
   // init hide all obj details
   $(".schema").hide();
   $(".field .props").hide();
-  var first = $(".objects li:first");
-  first.addClass('active');
-  $(".schema[data-name='"+ $('a', first).attr('data-name')+"']").show();
+  // activate from anchor or first
+  if (anchor.length>1){
+    var obj = $(".objects li a[data-name='"+anchor+"']").parent();
+    var obj_name = anchor;
+  }else{
+    var obj = $(".objects li:first");
+    var obj_name = $('a', first).attr('data-name');
+  }
+  obj.addClass('active');
+  $(".schema[data-name='"+ obj_name +"']").show();
 
   //append events(hide,click) to listings
   $('.objects li a').live('click', function(){
-    // hide all others
+    // deactivate all
     $('.objects li.active').toggleClass('active');
     $(".schema:visible").toggle();
-    var obj_name = $(this).attr('data-name');
+    // activate
     $(this).parent().toggleClass('active');
-    $(".schema[data-name='"+obj_name+"']").toggle();
+    // hide connected schema
+    $(".schema[data-name='"+ $(this).attr('data-name')+"']").toggle();
   });
-  // open field
+
+  // open a field definition
   $('.field .head').live('click', function(){
     $(this).next('.props').toggle();
+  });
+
+  //focus search
+  $('#filter').focus();
+  // filter
+  $('#filter').keyup(function(){
+    var val = $(this).val();
+    $(".objects li").show()
+    $(".objects li a").has("[data-name^='"+ val +"']").parent().show();
+    $(".objects li a").not("[data-name^='"+ val +"']").parent().hide();
   });
 
 });

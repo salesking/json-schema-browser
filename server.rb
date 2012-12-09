f8e29528be71ad769b94bcc7e16dd39a0bfca547
Georg Leciejewski <gl@salesking.eu>
2012-12-09 12:57:49 +0100

    add table view

--- server.rb
+++ server.rb
@@ -7,11 +7,34 @@ require 'sinatra/partial'
 
 get '/' do
   @schemas = SK::Api::Schema.read_all('v1.0')
-  @object_names = @schemas.map{|i| i[:title].humanize }.sort
+  @sorted_schemas = sort_schemas_by_category(@schemas)
   haml :index
+  end
+
+get '/table' do
+  @schemas = SK::Api::Schema.read_all('v1.0')
+  @sorted_schemas = sort_schemas_by_category(@schemas)
+  haml :table
 end
 
 get '/schemas' do
   content_type :json
   SK::Api::Schema.read_all('v1.0').to_json
+end
+
+# TODO enhance the schema itself to support such a descition on object level
+def sort_schemas_by_category(schemas)
+  sorted_schemas=[]
+  categories = {
+    documents: %w(document invoice credit_note order estimate payment_reminder recurring line_item),
+    contacts: %w(client address),
+    templates: %w(pdf_template email_template text_template export_template),
+    system: %w(company user language sub auth_permission),
+    supportive: %w(tag email comment attachment export payment product)
+  }
+  categories.each do |cat, obj_types|
+    sorted_schemas << cat
+    sorted_schemas << schemas.select{|obj| obj_types.include?(obj[:title]) }.sort{|a,b| a[:title].humanize <=> b[:title].humanize }
+  end
+  sorted_schemas
 end

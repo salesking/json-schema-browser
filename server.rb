7c042e0395d4d6b9b8599b05605c08bc7f833f8e
Georg Leciejewski <gl@salesking.eu>
2014-05-16 12:33:39 +0200

    gem update

--- server.rb
+++ server.rb
@@ -9,7 +9,7 @@ get '/' do
   @schemas = SK::Api::Schema.read_all('v1.0')
   @sorted_schemas = sort_schemas_by_category(@schemas)
   haml :index
-  end
+end
 
 get '/table' do
   @schemas = SK::Api::Schema.read_all('v1.0')
@@ -20,7 +20,7 @@ end
 get '/schemas' do
   content_type :json
   SK::Api::Schema.read_all('v1.0').to_json
-  end
+end
 
 get '/changelog' do
   @content = File.read(params[:topic] + ".md")
@@ -32,10 +32,11 @@ def sort_schemas_by_category(schemas)
   sorted_schemas=[]
   categories = {
     documents: %w(document invoice credit_note order estimate payment_reminder recurring line_item divider_item sub_total_item),
-    contacts: %w(contact client address),
+    contacts: %w(contact address),
     templates: %w(pdf_template email_template text_template export_template),
     system: %w(company user language sub auth_permission),
-    supportive: %w(tag email comment attachment export payment product)
+    supportive: %w(task tag email comment attachment export payment product),
+    accounting: %w(account account_entry account_billing),
   }
   categories.each do |cat, obj_types|
     sorted_schemas << cat
